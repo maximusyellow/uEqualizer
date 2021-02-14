6f3d5e26a6362c37f5b136e97cdee3661d05a0d2
maximusyellow <yerrowranger@gmail.com>
2021-02-13 19:43:28 -0500

    fixed manifest + recyclerview

--- app/app/src/main/java/com/example/uequalizer/MainActivity.java
+++ app/app/src/main/java/com/example/uequalizer/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.uequalizer;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothHeadset;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,8 +23,19 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView.LayoutManager layoutManager;
     BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-    String[] deviceName = {"Headphone"};
 
+    String[] deviceName = {"Headphone"};
+    public List<String> getDevices() {
+        List<String> s = new ArrayList<String>();
+        for (BluetoothDevice bt : pairedDevices) {
+            s.add(bt.getName());
+        }
+        return s;
+    }
+
+    public String getConnectedDevice() {
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,11 +44,14 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(false);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        programAdapter = new ProgramAdapter(this, deviceName);
+        recyclerView.setAdapter(programAdapter);
+        List<String> t = getDevices();
+        for(String m : t) {
+            Log.d(m, "String");
+        }
     }
 
-    public void getDevices() {
-        List<String> s = new ArrayList<String>();
-        for (BluetoothDevice bt : pairedDevices)
-            s.add(bt.getName());
-    }
+
+
 }
--- app/app/src/main/java/com/example/uequalizer/ProgramAdapter.java
+++ app/app/src/main/java/com/example/uequalizer/ProgramAdapter.java
@@ -1,27 +1,50 @@
 package com.example.uequalizer;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.viewHolder> {
+public class ProgramAdapter extends RecyclerView.Adapter<ProgramAdapter.ViewHolder> {
 
+    Context context;
+    String[] deviceName;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView rowName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            rowName = itemView.findViewById(R.id.textView1);
+        }
+    }
+
+    public ProgramAdapter(Context context, String[] deviceName) {
+        this.context = context;
+        this.deviceName = deviceName;
+    }
 
 
     @NonNull
     @Override
-    public ProgramAdapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ProgramAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.device_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ProgramAdapter.viewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ProgramAdapter.ViewHolder holder, int position) {
+    holder.rowName.setText(deviceName[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return deviceName.length;
     }
 }
